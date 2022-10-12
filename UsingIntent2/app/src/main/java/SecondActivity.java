c2ea00ba0c1af44e728d1129ff00d1e74e7013a4
hanandestiarin <hananksndrian@gmail.com>
2022-10-12 19:43:41 +0700

    first

---
+++ UsingIntent2/app/src/main/java/SecondActivity.java
@@ -0,0 +1,10 @@
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.usingintent2.R;
+
+public class SecondActivity extends Activity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ UsingIntent2/app/src/main/java/com/example/usingintent2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.usingintent2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    int request_Code = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        startActivityForResult(new Intent("com.example.usingintent2.SecondActivity"), request_Code);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this,data.getData().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
